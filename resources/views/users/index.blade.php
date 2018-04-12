28437106a701739ac2e8345d8f906033a21fe5c8
Christian Baltazar <cbaltazarc@guanajuato.gob.mxbrew>
2018-04-12 15:28:08 -0500

    delete user issue

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -74,7 +74,7 @@
             }, 
             deleteUser(id, index) {
                 if(confirm("Are you sure?")){
-                    this.$http.delete('{{url("users")}}' + id).then(response => {
+                    this.$http.delete('{{url("users")}}' + '/' + id).then(response => {
                         this.users.splice(index, 1);
                         alert("User is gone!");
                     }, error => {
